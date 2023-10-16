b434f0547292d794cb179829531eb3629e29f71a
Tatyana <lioness1741@mail.ru>
2023-10-16 10:59:51 +0500

    ready1

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # ajs-homeworks-SymbolsIterators
-[![Build status](https://ci.appveyor.com/api/projects/status/qinqlfmlsgl2rusd?svg=true)](https://ci.appveyor.com/project/lioness1741/ajs-homeworks-SymbolsIterators)
+[![Build status](https://ci.appveyor.com/api/projects/status/49dvqxr0w2sjuo3y?svg=true)](https://ci.appveyor.com/project/lioness1741/ajs-homeworks-SymbolsIterators)
--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,16 +1,32 @@
-import Person from '../character';
-import Team from '../team';
+import Character from '../Character';
+import Team from '../Team';
 
-test('...', () => {
-  const char1 = new Person('Nikita', 'Bowman');
-  const char2 = new Person('Nikita', 'Daemon');
-  const char3 = new Person('Nikita', 'Magician');
-  const expected = new Team([char1, char2, char3]);
-  const testTeam = [];
+const char1 = {
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 1,
+  attack: 40,
+  defence: 10,
+};
 
-  for (const member of expected) {
-    testTeam.push(member);
-  }
+const char2 = {
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 1,
+  attack: 40,
+  defence: 10,
+};
 
-  expect(expected.innerTeam).toEqual(testTeam);
+test('Character test', () => {
+  const expected = char1;
+  const result = new Character(expected);
+  expect(result).toEqual(expected);
+});
+
+test('Team test', () => {
+  const expected = { characters: [new Character(char1), new Character(char2)] };
+  const result = new Team(new Character(char1), new Character(char2));
+  expect(result).toEqual(expected);
 });
